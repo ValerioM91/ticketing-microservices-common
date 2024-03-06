e5af3c9c13dd18be6384b0b5de01bd3183f06a83
Valerio Mattera <matteravalerio@gmail.com>
2024-03-06 14:49:06 +0000

    not found error update

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -3,8 +3,8 @@ import { CustomError } from './custom-error'
 export class NotFoundError extends CustomError {
   statusCode = 404
 
-  constructor() {
-    super('Route not found')
+  constructor(message?: string) {
+    super(message || 'Route not found')
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, NotFoundError.prototype)
